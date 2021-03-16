b71e8773034fbaedb8de101b0475a327076facde
Vishal Kainth <44121034+vishalkainth@users.noreply.github.com>
2021-03-16 14:04:42 -0400

    Add files via upload

---
+++ A1_VishalKainth.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    for (int i = 0;; i++)
+    {
+        n = i * i;
+        if ((n % 10) / 2 == 1)
+        {
+            n = n / 10;
+            if ((n % 10) / 2 == 1)
+            {
+                cout << "The two equal integers: " << i << endl;
+                cout << "Perfect Square: " << i * i << endl;
+                cout << "Last Digit of Perfect Square: " << (i * i) % 10 << endl;
+                cout << "Second to Last Digit of Perfect Square: " << (i * i) % 10 % 10 << endl;
+                cout << "\nCheck: Square root of the perfect square is " << sqrt(i * i) << ". This problem is impossbile to solve." << endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
